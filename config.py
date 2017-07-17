562169ec34979e5789498217707306aee333d7b5
YuhangSong <YuhangSong2017@gmail.com>
2017-07-17 11:32:26 -0400

    done

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 num_workers = 1
 enable_gsa = True
 logdir = '../../result/gmbrl_1/test_3/'
-dataset_path = '../../dataset/' + '3DPinball_d01.npz'
+dataset_path = '../../dataset/' + '3DPinball_d01_128.npz'
 
 # default
 gan_predict_interval = 0.1
-gan_size = 64
-gan_batchsize = 14
+gan_size = 64*2
+gan_batchsize = 64
