4392d2bd98ed2915c343bc7445b671c8e7b3fce1
YuhangSong <YuhangSong2017@gmail.com>
2017-07-17 11:40:44 -0400

    done

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 num_workers = 1
 enable_gsa = True
 logdir = '../../result/gmbrl_1/test_3/'
-dataset_path = '../../dataset/' + '3DPinball_d01_128.npz'
+dataset_path = '../../dataset/'
+dataset_name = '3DPinball_1_d01_128.npz'
+video_name = '3DPinball_1.mp4'
 
 # default
 gan_predict_interval = 0.1
