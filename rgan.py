44b5d450e45d27d4e8cfc9d1911ea8073186e480
YuhangSong2017 <YuhangSong2017@gmail.com>
2017-08-21 20:27:29 -0400

    w4-3

--- rgan.py
+++ rgan.py
@@ -22,8 +22,8 @@ import domains.all_domains as chris_domain
 import matplotlib.cm as cm
 
 CLEAR_RUN = False
-MULTI_RUN = 'w4-2'
-GPU = '2'
+MULTI_RUN = 'w4-3'
+GPU = '3'
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU
 #-------reuse--device
@@ -50,7 +50,7 @@ add_parameters(EXP = 'simple_gg')
 add_parameters(DOMAIN = '1Dflip') # 1Dgrid, 1Dflip, 2Dgrid,
 add_parameters(FIX_STATE = False)
 add_parameters(REPRESENTATION = chris_domain.VECTOR) # chris_domain.SCALAR, chris_domain.VECTOR, chris_domain.IMAGE
-add_parameters(GRID_SIZE = 5)
+add_parameters(GRID_SIZE = 10)
 
 '''domain dynamic'''
 if params['DOMAIN']=='1Dflip':
