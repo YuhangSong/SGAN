c523f41ce2a19337d794c0c0553e3c8fc0e2bfec
YuhangSong2017 <YuhangSong2017@gmail.com>
2017-08-25 18:09:18 -0400

    try_5x5_low_dim_real_prob

--- rgan.py
+++ rgan.py
@@ -22,8 +22,8 @@ import domains.all_domains as chris_domain
 import matplotlib.cm as cm
 
 CLEAR_RUN = False
-MULTI_RUN = 'try_5x5_low_dim_same_prob'
-GPU = '0'
+MULTI_RUN = 'try_5x5_low_dim_real_prob'
+GPU = '2'
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU
 #-------reuse--device
@@ -46,7 +46,7 @@ def add_parameters(**kwargs):
     params.update(kwargs)
 
 '''domain settings'''
-add_parameters(EXP = 'try_5x5_low_dim_same_prob')
+add_parameters(EXP = 'try_5x5_low_dim_real_prob')
 add_parameters(DOMAIN = '2Dgrid') # 1Dgrid, 1Dflip, 2Dgrid,
 add_parameters(FIX_STATE = False)
 add_parameters(REPRESENTATION = chris_domain.VECTOR) # chris_domain.SCALAR, chris_domain.VECTOR, chris_domain.IMAGE
@@ -63,11 +63,10 @@ elif params['DOMAIN']=='1Dgrid':
 
 elif params['DOMAIN']=='2Dgrid':
     # add_parameters(GRID_ACTION_DISTRIBUTION = [0.8, 0.0, 0.1, 0.1])
-    add_parameters(GRID_ACTION_DISTRIBUTION = [1.0/3.0,2.0/3.0, 0.0, 0.0])
-    add_parameters(OBSTACLE_POS_LIST = [])
-
-    # add_parameters(GRID_ACTION_DISTRIBUTION = [0.25,0.25,0.25,0.25])
     # add_parameters(OBSTACLE_POS_LIST = [])
+
+    add_parameters(GRID_ACTION_DISTRIBUTION = [0.25,0.25,0.25,0.25])
+    add_parameters(OBSTACLE_POS_LIST = [])
 
     # add_parameters(GRID_ACTION_DISTRIBUTION = [0.8, 0.0, 0.1, 0.1])
     # add_parameters(OBSTACLE_POS_LIST = [(2, 2)])
