58b930a1dc9ce31f9cb4e21fde2daf3b3db66099
YuhangSong2017 <YuhangSong2017@gmail.com>
2017-08-27 17:24:03 -0400

    5x5_cd_rs_f

--- rgan.py
+++ rgan.py
@@ -22,8 +22,8 @@ import domains.all_domains as chris_domain
 import matplotlib.cm as cm
 
 CLEAR_RUN = False
-MULTI_RUN = '4x4_cd_rs_nf'
-GPU = '1'
+MULTI_RUN = '5x5_cd_rs_f'
+GPU = '0'
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU
 #-------reuse--device
@@ -48,9 +48,9 @@ def add_parameters(**kwargs):
 '''domain settings'''
 add_parameters(EXP = '2x2_cd_rs')
 add_parameters(DOMAIN = '2Dgrid') # 1Dgrid, 1Dflip, 2Dgrid,
-add_parameters(FIX_STATE = False)
+add_parameters(FIX_STATE = True)
 add_parameters(REPRESENTATION = chris_domain.IMAGE) # chris_domain.SCALAR, chris_domain.VECTOR, chris_domain.IMAGE
-add_parameters(GRID_SIZE = 4)
+add_parameters(GRID_SIZE = 5)
 
 '''domain dynamic'''
 if params['DOMAIN']=='1Dflip':
