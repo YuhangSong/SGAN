c288dd1004b55f842562076bd90f268ca6a5fe2d
YuhangSong2017 <YuhangSong2017@gmail.com>
2017-08-27 22:30:38 -0400

    1x5_cd_rs_nf

--- rgan.py
+++ rgan.py
@@ -22,8 +22,8 @@ import domains.all_domains as chris_domain
 import matplotlib.cm as cm
 
 CLEAR_RUN = False
-MULTI_RUN = '1x5_cd_rs_f'
-GPU = '0'
+MULTI_RUN = '1x5_cd_rs_nf'
+GPU = '1'
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU
 #-------reuse--device
@@ -48,7 +48,7 @@ def add_parameters(**kwargs):
 '''domain settings'''
 add_parameters(EXP = '2x2_cd_rs')
 add_parameters(DOMAIN = '1Dgrid') # 1Dgrid, 1Dflip, 2Dgrid,
-add_parameters(FIX_STATE = True)
+add_parameters(FIX_STATE = False)
 add_parameters(REPRESENTATION = chris_domain.IMAGE) # chris_domain.SCALAR, chris_domain.VECTOR, chris_domain.IMAGE
 add_parameters(GRID_SIZE = 5)
 
