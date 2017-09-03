b73fb76b1deccea05ea7256a98084ab65fc03593
s <YuhangSong2017@gmail.com>
2017-09-03 09:37:26 -0400

    flip_nf

--- rgan.py
+++ rgan.py
@@ -24,7 +24,7 @@ import imageio
 from decision_tree import *
 
 CLEAR_RUN = False # if delete logdir and start a new run
-MULTI_RUN = 'flip_f' # display a tag before the result printed
+MULTI_RUN = 'flip_nf' # display a tag before the result printed
 GPU = "0" # use which GPU
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU
@@ -50,7 +50,7 @@ def add_parameters(**kwargs):
 '''domain settings'''
 add_parameters(EXP = 'marble') # the first level of log dir
 add_parameters(DOMAIN = '1Dflip') # 1Dflip, 1Dgrid, 2Dgrid, marble
-add_parameters(FIX_STATE = True) # whether to fix the start state at a specific point, this will simplify training. Usually using it for debugging so that you can have a quick run.
+add_parameters(FIX_STATE = False) # whether to fix the start state at a specific point, this will simplify training. Usually using it for debugging so that you can have a quick run.
 add_parameters(REPRESENTATION = chris_domain.VECTOR) # chris_domain.SCALAR, chris_domain.VECTOR, chris_domain.IMAGE
 add_parameters(GRID_SIZE = 5) # size of 1Dgrid, 1Dflip, 2Dgrid
 
