4cca61270ebcc44cd95d4afc62a5e4dbcd54d6b8
YuhangSong2017 <YuhangSong2017@gmail.com>
2017-09-03 21:40:08 +0800

    bayes

--- rgan.py
+++ rgan.py
@@ -24,7 +24,7 @@ import imageio
 from decision_tree import *
 
 CLEAR_RUN = False # if delete logdir and start a new run
-MULTI_RUN = 'marble' # display a tag before the result printed
+MULTI_RUN = 'bayes' # display a tag before the result printed
 GPU = "0" # use which GPU
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU
