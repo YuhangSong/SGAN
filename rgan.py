09aba6006837620c85e40e7865dd3d42aec8b610
s <YuhangSong2017@gmail.com>
2017-09-06 07:21:33 -0400

    reward noise log 0.5

--- rgan.py
+++ rgan.py
@@ -24,8 +24,8 @@ import imageio
 from decision_tree import *
 
 CLEAR_RUN = False # if delete logdir and start a new run
-MULTI_RUN = 'reward noise log' # display a tag before the result printed
-GPU = "1" # use which GPU
+MULTI_RUN = 'reward noise log 0.5' # display a tag before the result printed
+GPU = "0" # use which GPU
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU # this is a lable displayed before each print and log, to identify different runs at the same time on one computer
 os.environ["CUDA_VISIBLE_DEVICES"] = GPU # set env variable that make the GPU you select
@@ -123,7 +123,7 @@ add_parameters(INTERPOLATES_MODE = 'auto') # auto, one
 # add_parameters(NOISE_ENCOURAGE = False)
 add_parameters(NOISE_ENCOURAGE = True)
 
-add_parameters(NOISE_ENCOURAGE_FACTOR = 1.0)
+add_parameters(NOISE_ENCOURAGE_FACTOR = 0.5)
 
 '''
 compute delta for differant domains
