48a8f3b0ac5168a1f3d99c47bd57f41499835e59
YuhangSong2017 <YuhangSong2017@gmail.com>
2017-09-07 02:06:22 -0400

    smaller_noise_dim

--- rgan.py
+++ rgan.py
@@ -24,8 +24,8 @@ import imageio
 from decision_tree import *
 
 CLEAR_RUN = False # if delete logdir and start a new run
-MULTI_RUN = 'low data, size 5, bigger noise, comp' # display a tag before the result printed
-GPU = "2" # use which GPU
+MULTI_RUN = 'smaller_noise_dim' # display a tag before the result printed
+GPU = "0" # use which GPU
 
 MULTI_RUN = MULTI_RUN + '|GPU:' + GPU # this is a lable displayed before each print and log, to identify different runs at the same time on one computer
 os.environ["CUDA_VISIBLE_DEVICES"] = GPU # set env variable that make the GPU you select
@@ -100,17 +100,17 @@ method settings
 '''
 add_parameters(METHOD = 's-gan') # tabular, bayes-net-learner, deterministic-deep-net, s-gan
 
-# add_parameters(GP_MODE = 'pure-guide') # none-guide, use-guide, pure-guide
-add_parameters(GP_MODE = 'none-guide') # none-guide, use-guide, pure-guide
+add_parameters(GP_MODE = 'pure-guide') # none-guide, use-guide, pure-guide
+# add_parameters(GP_MODE = 'none-guide') # none-guide, use-guide, pure-guide
 add_parameters(GP_GUIDE_FACTOR = 1.0)
 
-# add_parameters(INTERPOLATES_MODE = 'auto') # auto, one
-add_parameters(INTERPOLATES_MODE = 'one') # auto, one
+add_parameters(INTERPOLATES_MODE = 'auto') # auto, one
+# add_parameters(INTERPOLATES_MODE = 'one') # auto, one
 
 # add_parameters(NOISE_ENCOURAGE = False)
 add_parameters(NOISE_ENCOURAGE = True)
 
-add_parameters(NOISE_ENCOURAGE_FACTOR = 0.6)
+add_parameters(NOISE_ENCOURAGE_FACTOR = 1.0)
 
 '''
 compute delta for differant domains
@@ -161,7 +161,7 @@ else:
 
 '''model settings'''
 add_parameters(DIM = 128) # warnning: this is not likely to make a difference, but the result I report is on DIM = 512
-add_parameters(NOISE_SIZE = 128)
+add_parameters(NOISE_SIZE = 8)
 add_parameters(BATCH_SIZE = 32)
 add_parameters(DATASET_SIZE = 33554) # 1610612736
 # LAMBDA is set seperatly for different representations
